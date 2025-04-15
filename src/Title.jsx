5fe1e0920b0778b1c7bb831182373a24b7ff6128
Daniel Lopes <daniel.marqueslopes74@gmail.com>
2025-04-15 23:00:05 +0200

    trying react

---
+++ src/Title.jsx
@@ -0,0 +1,13 @@
+function Title(){
+
+    let name="Daniel Lopes"
+    return (
+    <div>
+    
+    <h1>My first title in React :D and my name is {name}</h1>
+    <p>Im trying to make an paragraph inside my function Title and with a variable let or cons to try something out </p>
+        
+        </div>)
+}
+
+export default Title
